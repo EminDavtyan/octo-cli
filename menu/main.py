201ef249519af4394a167280a8a1d2ca608e2252
Emin <edavdyan16@gmail.com>
2024-10-21 21:15:59 +0400

    Session, Capsule and Component creating

--- menu/main.py
+++ menu/main.py
@@ -1,10 +1,12 @@
 from collections import deque
-from abc import ABCMeta, abstractstaticmethod
+from abc import ABCMeta, abstractstaticmethod, abstractmethod
+from pynput import keyboard
 
 class Session(metaclass=ABCMeta):
     __instance = None
     history = deque()
-    currentCapsule = history[-1]
+    indicator = None
+    currentCapsule = None
 
     @classmethod
     def get_instance(cls):
@@ -12,20 +14,84 @@ class Session(metaclass=ABCMeta):
             cls.__instance = cls
         return cls.__instance
 
-    def control(self):
+    @classmethod
+    def define_capsule(cls, title):
+        cls.currentCapsule = Capsule(cls, title)
+        return cls.currentCapsule
+
+    @classmethod
+    def control(cls):
         """ Control of menu """
+        def __on_press(key):
+            try:
+                print('alphanumeric key {0} pressed'.format(key.char))
+            except AttributeError:
+                print('special key {0} pressed'.format(key))
+
+        def __on_release(key):
+            print('{0} released'.format(
+                key))
+            if key == keyboard.Key.esc:
+                # Stop listener
+                return False
+
+        with keyboard.Listener(
+                on_press=__on_press,
+                on_release=__on_release) as listener:
+            listener.join()
+
+    @classmethod
+    def run(cls):
+        print(cls.currentCapsule.components)
 
 
 class Capsule:
-    def __init__(self, title):
+    def __init__(self, session: Session, title: str):
+        assert session, 'Session not created'
         self.title = title
         self.components = deque()
+        self.status = False
+
+    def add(self, component_type, status: bool, label: str):
+        component = Component(component_type, status, label)
+        self.components.append(component)
 
 class Component:
-    def __init__(self):
-        pass #type, clickable, position
-class SimpleButton:
-    def __init__(self, parent: Capsule, title):
-        self.title = title
-        parent.components.append(self)
+    available_components = ['Label', 'SimpleButton']
 
+    def __init__(self, component_type: available_components, status: bool, label: str):
+        self.component_type = component_type
+        self.status = status
+        self.label = label
+
+    def focussed(self):
+        """ Design and animation of component while focused """
+        pass
+
+    def operating_on(self):
+        """ Component functionality """
+        pass
+
+    def __repr__(self):
+        return f'(Type: {self.component_type}, Status: {self.status}, Label: "{self.label}")'
+
+class Label(Component):
+    def __init__(self, parent_capsule: Capsule, text):
+        super().__init__(component_type='Label', label=text, status=True)
+        self.text = text
+        parent_capsule.components.append(self)
+
+    def __repr__(self):
+        return self.text
+
+class SimpleButton(Component):
+    def __init__(self, parent_capsule: Capsule, title):
+        super().__init__(component_type='SimpleButton')
+        self.title = title
+        parent_capsule.components.append(self)
+
+s = Session()
+c1 = s.define_capsule('Main')
+c1.add('Label', True, 'Label1')
+c1.add('Label', True, 'Label2')
+s.run()
